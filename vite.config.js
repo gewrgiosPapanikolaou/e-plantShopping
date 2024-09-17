d54bc6467c131dda7e5ac9890b7d9d77f30ce0f6
George Papanikolaou <gewrgios.papanikolaou@gmail.com>
2024-09-17 02:59:14 -0400

    publish

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/gewrgiosPapanikolaou/e-plantShopping",
+  base: "https://gewrgiospapanikolaou.github.io/e-plantShopping/",
   plugins: [react()],
 })
